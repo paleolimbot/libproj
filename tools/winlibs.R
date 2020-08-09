84d8f62cb625ada07d8d429ae4965fdcdc40778a
Dewey Dunnington <dewey@fishandwhistle.net>
2020-08-09 17:56:59 -0300

    attempt to also link libcurl

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -7,3 +7,13 @@ if (!file.exists("../windows/libtiff-4.1.0/mingw64/include/tiff.h")) {
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
 }
+
+# Build against static libraries from rwinlib
+VERSION <- commandArgs(TRUE)
+if(!file.exists(sprintf("../windows/libcurl-%s/include/curl/curl.h", VERSION))){
+  if(getRversion() < "3.3.0") setInternet2()
+  download.file(sprintf("https://github.com/rwinlib/libcurl/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
+  dir.create("../windows", showWarnings = FALSE)
+  unzip("lib.zip", exdir = "../windows")
+  unlink("lib.zip")
+}
