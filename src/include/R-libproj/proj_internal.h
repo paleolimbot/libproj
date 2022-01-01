be9936aa2cad0a2945381936e6af77a7d0c5ebe2
Dewey Dunnington <dewey@fishandwhistle.net>
2022-01-01 14:12:31 -0400

    fix ARG_list typdef

--- src/include/R-libproj/proj_internal.h
+++ src/include/R-libproj/proj_internal.h
@@ -552,8 +552,8 @@ struct PJconsts {
     double  datum_params[7] = {0,0,0,0,0,0,0}; /* Parameters for 3PARAM and 7PARAM */
 
     int     has_geoid_vgrids = 0;      /* used by legacy transform.cpp */
-    void*   hgrids_legacy = nullptr;   /* used by legacy transform.cpp. Is a pointer to a ListOfHGrids* */ 
-    void*   vgrids_legacy = nullptr;   /* used by legacy transform.cpp. Is a pointer to a ListOfVGrids* */ 
+    void*   hgrids_legacy = nullptr;   /* used by legacy transform.cpp. Is a pointer to a ListOfHGrids* */
+    void*   vgrids_legacy = nullptr;   /* used by legacy transform.cpp. Is a pointer to a ListOfVGrids* */
 
     double  from_greenwich = 0.0;       /* prime meridian offset (in radians) */
     double  long_wrap_center = 0.0;     /* 0.0 for -180 to 180, actually in radians*/
@@ -597,13 +597,20 @@ struct PJconsts {
 struct ARG_list {
     paralist *next;
     char used;
-#if (defined(__GNUC__) && __GNUC__ >= 8) || (defined(__clang__) && __clang_major__ >= 9)
-    char param[]; /* variable-length member */
-    /* Safer to use [] for gcc 8. See https://github.com/OSGeo/proj.4/pull/1087 */
-    /* and https://gcc.gnu.org/bugzilla/show_bug.cgi?id=86914 */
-#else
-    char param[1]; /* variable-length member */
-#endif
+    // this causes CRAN checks to fail and has been backported to most
+    // versions of GCC8 in use. We need param[64] because the sanitizer checks
+    // for address errors outside the bounds of param[1] (which is literally
+    // every parameter). This will add 64 bytes of overhead to every +proj=XXX param
+    // which should be trivial as the overhead of creating projections is not linked
+    // to the parameter list. It should be noted that arguments can still be more than
+    // 64 bytes because struct ARG_list is always allocated as sizeof(paralist) + strlen(param) + 1.
+    // #if (defined(__GNUC__) && __GNUC__ >= 8) || (defined(__clang__) && __clang_major__ >= 9)
+    //    char param[]; /* variable-length member */
+    //    /* Safer to use [] for gcc 8. See https://github.com/OSGeo/proj.4/pull/1087 */
+    //    /* and https://gcc.gnu.org/bugzilla/show_bug.cgi?id=86914 */
+    // #else
+    char param[64]; /* variable-length member */
+// #endif
 };
 
 
